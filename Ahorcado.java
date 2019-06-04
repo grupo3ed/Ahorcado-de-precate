2afaf3b42cae0c4453c56d4ec132e4f25271d1a4
theGcarrer <49192786+theGcarrer@users.noreply.github.com>
2019-06-04 08:55:10 +0200

    Create Ahorcado.java
    si, se parece mucho a uno, porque me he basado en el para hacerlo, el caso es que funciona y se puede vender al publico

---
+++ Ahorcado.java
@@ -0,0 +1,199 @@
+package prueba;
+ 
+import java.util.Scanner;
+public class ahorcado {
+ 
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int vidas = 6;int aciertos = 0;
+        String palabra;
+        
+        int tamaño;
+        String opcion;
+        int contador = 0;
+        String[] palabras = new String[]{"anivia", "shyvana", "teemo", "warwick", "kayn", "ornn"};
+        char[] respuesta;
+        palabra = palabras[(int) (Math.random() * 5)];
+        tamaño = palabra.length();
+        respuesta = new char[tamaño];
+         
+         
+        for (int i = 0; i < tamaño; i++) {
+            respuesta[i] = 'X';
+        }
+         
+ 
+        while (aciertos != tamaño && vidas != 0) {
+            System.out.println("=========AHORCADO en Java:)==========          VIDAS="+vidas +" Aciertos= "+aciertos);
+            dibujar(vidas);
+            System.out.println("");
+            for (int i = 0; i < tamaño; i++) {
+ 
+                System.out.print("__" + respuesta[i] + "___  ");
+            }
+            System.out.println("\nIngresa una letra: ");
+            opcion = s.next();
+            if (palabra.contains(opcion)) {
+                for (int i = 0; i < tamaño; i++) {
+                    if (palabra.charAt(i) == opcion.charAt(0)) {
+                        respuesta[i] = opcion.charAt(0);
+                        contador++;
+                    }
+                }
+ 
+                aciertos = aciertos + contador;
+            } else {
+                vidas--;
+            }
+ 
+            contador = 0;
+        }
+        if(vidas==0){
+            dibujar(vidas);
+        }else
+        {
+            System.out.println("");
+            for (int i = 0; i < tamaño; i++) {
+ 
+                System.out.print("__" + respuesta[i] + "___  ");
+            }
+            System.out.println("YOU WIN :)");
+             
+            s.close();}
+    }
+ 
+    private static void dibujar(int i) {
+        switch (i) {
+            case 6:
+                System.out.println(" ---------------------");
+                for (int j = 0; j < 15; j++) {
+                    System.out.println(" |");
+ 
+                }
+                System.out.println("__________");
+                break;
+ 
+            case 5:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | X   X |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                for (int j = 0; j < 10; j++) {
+                    System.out.println(" |");
+ 
+                }
+                System.out.println("__________");
+                break;
+ 
+            case 4:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | X   X |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(" |");
+ 
+                }
+                System.out.println("__________");
+                break;
+ 
+            case 3:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | X   X |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / |   ");
+                System.out.println(" |                 /   |   ");
+                System.out.println(" |                /    |   ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(" |");
+ 
+                }
+                System.out.println("__________");
+                break;
+ 
+            case 2:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | X   X |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / | \\ ");
+                System.out.println(" |                  /  |   \\ ");
+                System.out.println(" |                 /   |     \\ ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(" |");
+ 
+                }
+                System.out.println("__________");
+                break;
+ 
+            case 1:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | X   X |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / | \\ ");
+                System.out.println(" |                  /  |   \\ ");
+                System.out.println(" |                 /   |     \\ ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                    /  ");
+                System.out.println(" |                   /      ");
+                System.out.println(" |                  /       ");
+                for (int j = 0; j < 2; j++) {
+                    System.out.println(" |");
+ 
+                }
+                System.out.println("__________");
+                break;
+ 
+            case 0:
+               System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | X   X |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / | \\ ");
+                System.out.println(" |                  /  |   \\ ");
+                System.out.println(" |                 /   |     \\ ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                    / \\");
+                System.out.println(" |                   /   \\  ");
+                System.out.println(" |                  /     \\ ");
+                for (int j = 0; j < 2; j++) {
+                    System.out.println(" |");
+ 
+                }
+                System.out.println("__________");
+                System.out.println("GAME OVER");
+                break;
+        }
+    }
+}
